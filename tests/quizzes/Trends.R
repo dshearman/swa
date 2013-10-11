070c1966892c50a04f6bdf0251fda2758caeab56
Glenn Stone <g.stone@uws.edu.au>
2013-10-11 16:00:49 +1100

    Added Trends quiz

---
+++ tests/quizzes/Trends.R
@@ -0,0 +1,68 @@
+questions = list(
+  list(
+    type =  'NUM',
+    known.parameters =  'X Y',
+    question = '<p>Compute the simplyfied Twitter chi-squared statistic (without the second term) given that the tweets in the before period are `r X` and in the current period `r Y`</p>',
+    answer = '`r (Y-X)^2/X`',
+    tolerance = "0.05"
+  ),
+  list(
+    type =  'NUM',
+    known.parameters =  'SSX SSXY RSS n',
+    question = '<p>When conducting a linear regression, on the square root of `r n` twitter counts, the following summary statistics were obtained. SSX = `r SSX`, SSXY = `r SSXY` and RSS = `r RSS`. Calculate the t-statistic for the test of the slope (&beta;) being zero.</p>',
+    answer = '`r (SSXY/SSX)/sqrt(RSS/((n-2)*SSX))`',
+    tolerance = "0.05"
+  ),
+  list(
+    type =  'NUM',
+    known.parameters =  'y',
+    question = '<p>It is beleived that there is a seasonal cycle of period 4 in a set of count data. The following
+    observations are the square roots of part of the data. Calculate the 4 point moving average for the 
+    third observation.</p> <p>`r y`</p>',
+    answer = '`r (0.5*(y[1]+y[5])+sum(y[2:4]))/4`',
+    tolerance = "0.05"
+  ),
+  list(
+    type =  'NUM',
+    known.parameters =  's',
+    question = '<p>It is beleived that there is a seasonal cycle of period 4 in a set of count data. The following
+    are three of the seasonal components. Calculate the fourth component.</p> <p>`r s`</p>',
+    answer = '`r  -sum(s)`',
+    tolerance = "0.05"
+  )
+  
+)
+
+
+set.seed(1725)
+for(i in 1:4) yaml.out(questions[[1]], 'X=rpois(1,357); Y=rpois(1,422)')
+
+set.one = function(n) {
+  x = runif(n)
+  y = 1.7*x + rnorm(length(x))
+  SSX = sum((x-mean(x))^2)
+  SSXY = sum((x-mean(x))*(y-mean(y)))
+  fit = lm(y~x)
+  RSS = sum(residuals(fit)^2)
+  paste(c("n", "SSX", "SSXY", "RSS"),
+  c(formatC(n, format="d"), formatC(c(SSX, SSXY, RSS), format="f", digits=3)),
+        sep="=", collapse=";")
+}
+
+
+set.seed(8872)
+for(n in c(20,22,23,25)) yaml.out(questions[[2]], set.one(n))
+
+count = c(135, 145, 133, 102, 105, 108, 128, 144, 149, 130, 107, 106, 83, 117, 
+          123, 104, 116, 127, 75, 128, 136, 145, 120, 127, 110, 109, 122, 136, 125, 
+          143, 177, 142, 134, 153, 173, 151, 174, 168, 158, 156, 128, 156, 140, 133, 
+          132)
+Y = sqrt(count)
+
+for(i in c(1,7,13,19)) {
+  str = paste("y=c(", paste(formatC(Y[i:(i+5)], format="f", digits=2), collapse=","),")")
+  yaml.out(questions[[3]], str)
+}
+
+set.seed(78867)
+for(i in 1:4) yaml.out(questions[[4]], "s=round(rnorm(3),3)")
