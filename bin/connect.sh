5ef69a2e2403fd6c03a5e20eaae12c30b857e199
Laurence Park <lapark@scem.uws.edu.au>
2014-08-05 10:57:20 +1000

    Added quiz questions.

--- bin/connect.sh
+++ bin/connect.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cadaver https://vuws.uws.edu.au/bbcswebdav/courses/300958_2013_spr/Lectures
+#cadaver https://vuws.uws.edu.au/bbcswebdav/courses/300958_2013_spr/Lectures
+cadaver https://vuws.uws.edu.au/bbcswebdav/courses/300958_2014_spr/Lectures
