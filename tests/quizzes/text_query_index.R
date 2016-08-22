44e1f1a4a34bc25a9fa958f14564a357b0a8d142
Laurence Park <lapark@scem.uws.edu.au>
2016-08-22 12:57:23 +1000

    Updated quiz to contain new material.

--- tests/quizzes/text_query_index.R
+++ tests/quizzes/text_query_index.R
@@ -3,10 +3,10 @@
 questions = list(
     list(
       type =  'NUM',
-      known.parameters =  "p x n",
-      question = 'The probability of finding the word "test" in a document collection is `r p`. Use Divergence from Random weighting with the binomial distribution to compute the weight of the word "test", in a document of length `r n`, where "test" appears `r x` times.',
-      answer = '`r -log(dbinom(x,n,p))`',
-      tolerance = "0.1"
+      known.parameters =  "p k n alpha",
+      question = 'The probability P(t|d) for word t in document d is `r p`, and the probability P(t|C) for word t in the document collection C is `r k`. Given that the document is `r n` words long, compute the Dirichlet Smoothed probability of word t in document d using alpha = `r alpha`. Provide your answer using at least two decimal places.',
+      answer = '`r lambda = n/(n + alpha); d = lambda*p + (1-lambda)*k; d`',
+      tolerance = "0.02"
     ),
     list(
       type =  'NUM',
@@ -18,7 +18,7 @@ questions = list(
     list(
       type =  'MC',
       known.parameters =  "q d",
-      question = 'A query has been converted into the following vector `r paste(c("[", q, "]"), collapse=" ")`. Which of the following document vectors is most similar to the given query vector (using Cosine similarity)?',
+      question = 'A query has been converted into the following vector `r paste(c("[", q, "]"), collapse=" ")`. Which of the following document vectors is most similar to the given query vector, using Cosine similarity? (Do not apply TF-IDF weights, use the given vector values).',
       choices = list(
             '`r paste(c("[", d[1,], "]"), collapse=" ")`',
             '`r paste(c("[", d[2,], "]"), collapse=" ")`',
@@ -129,15 +129,14 @@ questions = list(
 )
 
 
-
-yaml.out(questions[[1]], "x = 3; n = 40; p = 0.1")
-yaml.out(questions[[1]], "x = 1; n = 50; p = 0.2")
-yaml.out(questions[[1]], "x = 2; n = 60; p = 0.1")
-yaml.out(questions[[1]], "x = 4; n = 70; p = 0.2")
-yaml.out(questions[[1]], "x = 3; n = 50; p = 0.25")
-yaml.out(questions[[1]], "x = 1; n = 40; p = 0.3")
-yaml.out(questions[[1]], "x = 2; n = 70; p = 0.25")
-yaml.out(questions[[1]], "x = 4; n = 60; p = 0.3")
+yaml.out(questions[[1]], "p = 0.2; k = 0.1; n = 80; alpha = 10")
+yaml.out(questions[[1]], "p = 0.2; k = 0.1; n = 70; alpha = 1")
+yaml.out(questions[[1]], "p = 0.1; k = 0.2; n = 60; alpha = 5")
+yaml.out(questions[[1]], "p = 0.1; k = 0.2; n = 50; alpha = 15")
+yaml.out(questions[[1]], "p = 0.05; k = 0.2; n = 50; alpha = 1")
+yaml.out(questions[[1]], "p = 0.05; k = 0.2; n = 60; alpha = 10")
+yaml.out(questions[[1]], "p = 0.3; k = 0.1; n = 70; alpha = 5")
+yaml.out(questions[[1]], "p = 0.3; k = 0.1; n = 80; alpha = 15")
 
 yaml.out(questions[[2]], "fdt = 3; N = 90; ft = 2")
 yaml.out(questions[[2]], "fdt = 1; N = 50; ft = 4")
