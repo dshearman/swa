573c923b0d20b648a972da93f47e15260e228b22
Laurence Park <lapark@scem.uws.edu.au>
2016-09-06 12:37:07 +1000

    Updated week 8 lecture.

--- projects/upload.sh
+++ projects/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-(head -n 2 company_name2015.txt && tail -n +3 company_name2015.txt | sort) > company_name2015s.txt
-scp company_name2015s.txt staff.scem.uws.edu.au:~/Homepage/300958/project/company_name2015.txt
+(head -n 2 company_name2015.txt && tail -n +3 company_name2016.txt | sort) > company_name2016s.txt
+scp company_name2016s.txt staff.scem.uws.edu.au:~/Homepage/300958/project/company_name2016.txt
